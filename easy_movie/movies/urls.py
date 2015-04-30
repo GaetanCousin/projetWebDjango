36ff87704a00a6b8c0d86f06b123d403825ba048
GaetanCousin <cousin.gaetan@laposte.net>
2015-04-30 14:44:59 +0200

    MAJ views.py urls.py

--- easy_movie/movies/urls.py
+++ easy_movie/movies/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns("movies.views",
+urlpatterns = patterns("movies.views", # movies/toto indice 0 et la View indice 1
 	url(r"^$","home"),
+	#url(r"^(?P<id>\d+)", "movie"), # SOUCIS EN PARLER A MATTHIEU
+	url(r"notation", "notation"),
+	url(r"connection", "connection"),
+	url(r"inscription", "inscription"),
+	url(r"reponse", "reponseRecherche"),
+	url(r"espacePerso", "espacePerso"),
+	url(r"aPropos", "aPropos")
 	)
--- easy_movie/movies/views.py
+++ easy_movie/movies/views.py
@@ -2,29 +2,53 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User
 from movies.models import Category, Actor, Producer, Realisator, Movie, MovieState
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-	#if request.GET:
+	movies = Movie.objects.get(id=1)
+	#def get_mark(movie):
+		#movies = MovieState.objects.select(movie = movie)
+		#marks = [x.note for x in movies]
+		#return sum(marks)/len(marks)
+	#movies = Movie.objects.all()
+	#movies = sorted([(get_mark(x), x) for x in movies])
+	#movies = movies[-5:]
+	return render(request, r"movies/index.html", {"movies" : movies})
+
+@login_required
+def notation(request):
+	if request.GET:
 		try:
 			movie = Movie.objects.get(id=1)
 			user = User.objects.get(id=1)
 			rel = MovieState.objects.get(movie=movie, user=user)
 		except:
 			rel = []
-		print(rel)
 		return render(request, r"movies/notation.html", {"rel":rel})
-	#elif request.POST:
-	#	movie = 
-	#	note = 
-	#	new_rel = MovieState(note=note, user=user, movie=movie)
-	#	new_rel.save()
+	elif request.POST:
+		user = Movie.object.get(id=1)
+		movie = request.POST['titre']
+		note = request.POST['note']
+		new_rel = MovieState(note=note, user=user, movie=movie)
+		new_rel.save()
+	return render(request, r"movies/notation.html")
 
-def index(request):
-	def get_mark(movie):
-		movies = MovieState.objects.select(movie = movie)
-		marks = [x.note for x in movies]
-		return sum(marks)/len(marks)
-	movies = Movie.objects.all()
-	movies = sorted([(get_mark(x), x) for x in movies])
-	movies = movies[-5:]
-	return render(request, r"movies/index.html", {"movies" : movies})
+def connection(request):    # Plusieurs fois la même fonction juste pour initialiser les views 
+	movies = Movie.objects.get(id=1)
+	return render(request, r"movies/connection.html", {"movies" : movies})
+
+def inscription(request):
+	movies = Movie.objects.get(id=1)
+	return render(request, r"movies/inscription.html", {"movies" : movies})
+@login_required
+def reponseRecherche(request):
+	movies = Movie.objects.get(id=1)
+	return render(request, r"movies/reponseRecherche.html", {"movies" : movies})
+@login_required
+def espacePerso(request):
+	movies = Movie.objects.get(id=1)
+	return render(request, r"movies/espacePerso.html", {"movies" : movies})
+
+def aPropos(request):
+	return render(request, r"movies/aPropos.html")
