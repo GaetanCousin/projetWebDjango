edcb155f0523d8406e636b2c7b3cb24e40f0bb78
GaetanCousin <cousin.gaetan@laposte.net>
2015-05-02 13:44:58 +0200

    modif views

--- easy_movie/movies/views.py
+++ easy_movie/movies/views.py
@@ -6,15 +6,15 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-	movies = Movie.objects.get(id=1)
-	#def get_mark(movie):
-		#movies = MovieState.objects.select(movie = movie)
-		#marks = [x.note for x in movies]
-		#return sum(marks)/len(marks)
-	#movies = Movie.objects.all()
-	#movies = sorted([(get_mark(x), x) for x in movies])
-	#movies = movies[-5:]
-	return render(request, r"movies/index.html", {"movies" : movies})
+	#movies = Movie.objects.get(id=1)
+	def get_mark(movie):
+		movies = MovieState.objects.select(movie= movie) # erreur a ce niveau la je ne comprend pas pourquoi, ca fonctionne avec movies = MovieState.objects.all() mais ce n'est pas ce qu'on veut .. :S
+		marks = [x.note for x in movies]
+		return sum(marks)/len(marks) # len = nbr d'element
+	movies = Movie.objects.all()
+	moviesTrie = sorted([(get_mark(x), x.id) for x in movies])
+	topMovies = moviesTrie[-5:]
+	return render(request, r"movies/index.html", {"topMovies" : topMovies})
 
 @login_required
 def notation(request):
@@ -39,8 +39,17 @@ def connection(request):    # Plusieurs fois la même fonction juste pour initia
 	return render(request, r"movies/connection.html", {"movies" : movies})
 
 def inscription(request):
-	movies = Movie.objects.get(id=1)
-	return render(request, r"movies/inscription.html", {"movies" : movies})
+	if request.GET:
+		return render(request, r"movies/inscription.html")
+	elif request.POST:
+		adresse = request.POST['adresse']
+		mdp = request.POST['mdp']
+		new_user = User(username=adresse, mdp=mdp)
+		new_user.save()
+	return render(request, r"movies/inscription.html")
+
+
+
 @login_required
 def reponseRecherche(request):
 	movies = Movie.objects.get(id=1)
